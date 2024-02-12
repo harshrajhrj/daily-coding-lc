638decbf634acf3c14aa76b16a29f3fa6a167f4b
rajharshcr7 <rajharsh.footballer@gmail.com>
2024-02-12 22:17:45 +0530

    Feb 2024 --solution added

--- February 2024/12. Majority Element/solution.cpp
+++ February 2024/12. Majority Element/solution.cpp
@@ -5,18 +5,18 @@ public:
     {
         int n = nums.size();
         int freq = 0;
-        int i = 0, j = 0;
+        int major = 0, j = 0;
         while (j < n)
         {
             if (freq == 0)
-                i = nums[j];
-            if (nums[j] == i)
+                major = nums[j];
+            if (nums[j] == major)
                 freq++;
             else
                 freq--;
             j++;
         }
 
-        return i;
+        return major;
     }
 };
